adbd875af4ecca80851f0eb6ce11cf89a10f812a
phucpham09 <phamhuuphuc4049@gmail.com>
2025-04-16 07:28:22 +0700

    feat(auth): hiding auth function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-[calc(100vh-6rem)] gap-y-4">
       <p className="text-2xl text-gray-600">Welcome to MatchMe App</p>
       <Link
-        href={"/login"}
+        href={"/dashboard"}
         className="border font-semibold text-2xl text-gray-400 px-3 py-1 rounded-full cursor-pointer"
       >
         Get started!
--- src/components/Navbar/TopNav.tsx
+++ src/components/Navbar/TopNav.tsx
@@ -11,13 +11,13 @@ import { MdOutlineMessage } from "react-icons/md";
 const TopNav = () => {
   const [isMenu, setIsMenu] = useState(false);
   return (
-    <div className="flex sm:justify-between p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div className="flex sm:justify-center sm:items-center p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <IoMenu
         className="sm:hidden text-white w-1/12 place-self-start cursor-pointer"
         size={"40px"}
         onClick={() => setIsMenu(!isMenu)}
       />
-      <div className="flex justify-center items-center max-sm:w-11/12">
+      <div className="flex justify-center items-center max-sm:w-11/12 sm:fixed sm:left-6">
         <GiThreeFriends size={"40px"} className="text-white" />
         <h1 className="text-white text-2xl font-semibold place-self-end cursor-pointer">
           <Link href={"/dashboard"}>MatchMe</Link>
@@ -55,7 +55,7 @@ const TopNav = () => {
           Phòng chat
         </Link>
       </div>
-      <div className="sm:flex justify-center items-center gap-x-4 hidden">
+      {/* <div className="sm:flex justify-center items-center gap-x-4 hidden">
         <Link
           href={"/login"}
           className="border-white border-2 text-white rounded-full px-4 py-1 text-2xl"
@@ -68,7 +68,7 @@ const TopNav = () => {
         >
           Register
         </Link>
-      </div>
+      </div> */}
 
       {/* <div
         className={`fixed top-0 left-0 h-full w-2/3 bg-white shadow-lg transition-transform duration-300 ease-in-out ${
@@ -110,7 +110,7 @@ const TopNav = () => {
             </Link>
           </div>
         </div>
-      </div> */}   
+      </div> */}
       <div
         className={`fixed top-0 z-50 left-0 h-full w-2/3 bg-white shadow-lg transition-transform duration-300 ease-in-out ${
           isMenu ? "translate-x-0" : "-translate-x-full"
@@ -167,7 +167,7 @@ const TopNav = () => {
               Phòng chat
             </Link>
           </div>
-          <div
+          {/* <div
             className="flex justify-center items-center border-2 py-2 px-12 rounded-full"
             onClick={() => setIsMenu(!isMenu)}
           >
@@ -182,7 +182,7 @@ const TopNav = () => {
             <Link href={"/register"} className="text-2xl ">
               Register
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
